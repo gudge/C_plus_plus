4c4f225b8af04bd67b06a5ddcb8cfc2e7a728b85
Shankha Banerjee <shankha@cloudistics.com>
2016-04-10 14:41:56 -0400

    Functors

--- Functors.cpp
+++ Functors.cpp
@@ -2,5 +2,18 @@
 
 class X
 {
-
+    public:
+        void operator()() const;
 };
+
+void X::operator()() const
+{
+    std::cout << "Operator" << std::endl;
+}
+
+int
+main()
+{
+    const X x;
+    x();
+}
