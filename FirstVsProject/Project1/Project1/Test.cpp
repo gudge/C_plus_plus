7af22b62f418a81ca6a5bba9f032a559d5dc5a68
Shankha Banerjee <shankha@cloudistics.com>
2016-05-22 20:50:35 -0400

    Add project

---
+++ FirstVsProject/Project1/Project1/Test.cpp
@@ -0,0 +1,13 @@
+#include "Test.h"
+#include <iostream>
+using namespace std;
+
+Test::Test()
+{
+	std::cout << "Hello World" << std::endl;
+}
+
+
+Test::~Test()
+{
+}
---
+++ Test2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <memory>
+using namespace std;
+
+int
+main()
+{
+	std::cout << "Hello" << std::endl;
+	return 0;
+}
