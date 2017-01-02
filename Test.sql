a6af22675932f7fd1ffbeb8ecd25cebea6f68db6
Shankha Banerjee <shankha@cloudistics.com>
2017-01-02 18:42:46 -0500

    Jan 2

---
+++ Test.sql
@@ -0,0 +1,19 @@
+﻿DROP SCHEMA IF EXISTS s CASCADE;  
+CREATE SCHEMA s;
+
+CREATE TABLE s.cities (
+    name            text,
+    population      real,
+    altitude        int
+);
+
+CREATE TABLE s.capitals (
+    state           char(2)
+) INHERITS (s.cities);
+
+INSERT INTO s.capitals(state) values ('ab');
+
+-- SELECT * FROM s.capitals;
+SELECT * FROM s.cities;
+
+DROP SCHEMA IF EXISTS s CASCADE;  
