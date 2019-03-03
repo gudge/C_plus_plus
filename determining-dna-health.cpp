593061134c9bd56e700f54148a354093756aee25
Shankha Banerjee <shankha@cloudistics.com>
2019-03-03 15:40:05 -0500

    Initial changes

---
+++ determining-dna-health.cpp
@@ -0,0 +1,10 @@
+// https://www.hackerrank.com/challenges/determining-dna-health/problem
+
+// https://en.wikipedia.org/wiki/Aho%E2%80%93Corasick_algorithm
+// Boyer Moore
+// KMP String matching algorithm
+// Aho–Corasick_algorithm
+// Trie
+// Persistent segmentation tree
+//
+
